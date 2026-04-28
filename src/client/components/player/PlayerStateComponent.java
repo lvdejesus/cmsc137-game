186547a0806f639ce3e7e3cf06403507413abee0
Haraya Amihan S. Ching <harayaching@gmail.com>
2026-04-29 01:48:34 +0800

    feat: non functional state componenet added

---
+++ src/client/components/player/PlayerStateComponent.java
@@ -0,0 +1,19 @@
+package client.components.player;
+import framework.engine.Component;
+
+public class PlayerStateComponent implements Component{
+    public enum State {
+        IDLE,
+        MOVING,
+    };
+    public State current = State.IDLE;
+    public State previous = State.IDLE;
+
+    // Update State
+    public void set(State next){
+        if(this.current != next){
+            this.previous = this.current;
+            this.current = next ;
+        }
+    }
+}
